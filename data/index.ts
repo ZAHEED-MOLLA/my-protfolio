dc8ec7b2ef083b78bee7bcd2604add9a5faafc03
ZAHEED-MOLLA <zaheedhossain06@gmail.com>
2024-05-22 10:32:31 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a JS Animation library",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Chat Application",
+      des: "Explore the real time chat in this project",
+      img: "/pp1.png",
+      iconLists: ["/re.svg", "/tail.svg", "/javascript.svg", "/github.png", "/fm.svg"],
+      link: "https://chat-app-gtva.onrender.com",
+    },
+    {
+      id: 2,
+      title: "A Video Conferencing App",
+      des: "Simplify your video conferencing experience with Zoom. Seamlessly connect with colleagues and friends.",
+      img: "/pp2.png",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "https://nextjs-zoom-clone-tan.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "A Github Like Project",
+      des: "A website that using the latest tech stack.",
+      img: "/pp3.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/github.png", "/node-js.svg"],
+      link: "https://mern-github-app-vru7.onrender.com",
+    },
+    {
+      id: 4,
+      title: "Animated Ochi Website",
+      des: "Recreated the Ochi website, combining GSAP animations and Framer motion",
+      img: "/pp4.png",
+      iconLists: ["/re.svg", "/tail.svg", "/javascript.svg", "/gsap.svg"],
+      link: "https://ochi-zaheedmollas-projects.vercel.app/",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+      "I'm Zaheed, a dedicated web developer with a passion for creating exceptional digital experiences. My journey in web development began several years ago, and since then, I've honed my skills in various facets of development, from front-end design to back-end functionality. If you're seeking to elevate your website and brand, I am the ideal partner.",
+      name: "Zaheed Hossain Molla",
+      title: "Full Stack Developer",
+    },
+    {
+      quote:
+      "I'm Zaheed, a dedicated web developer with a passion for creating exceptional digital experiences. My journey in web development began several years ago, and since then, I've honed my skills in various facets of development, from front-end design to back-end functionality. If you're seeking to elevate your website and brand, I am the ideal partner.",
+      name: "Zaheed Hossain Molla",
+      title: "Full Stack Developer",
+    },
+    {
+      quote:
+      "I'm Zaheed, a dedicated web developer with a passion for creating exceptional digital experiences. My journey in web development began several years ago, and since then, I've honed my skills in various facets of development, from front-end design to back-end functionality. If you're seeking to elevate your website and brand, I am the ideal partner.",
+      name: "Zaheed Hossain Molla",
+      title: "Full Stack Developer",
+    },
+    {
+      quote:
+      "I'm Zaheed, a dedicated web developer with a passion for creating exceptional digital experiences. My journey in web development began several years ago, and since then, I've honed my skills in various facets of development, from front-end design to back-end functionality. If you're seeking to elevate your website and brand, I am the ideal partner.",
+      name: "Zaheed Hossain Molla",
+      title: "Full Stack Developer",
+    },
+    {
+      quote:
+      "I'm Zaheed, a dedicated web developer with a passion for creating exceptional digital experiences. My journey in web development began several years ago, and since then, I've honed my skills in various facets of development, from front-end design to back-end functionality. If you're seeking to elevate your website and brand, I am the ideal partner.",
+      name: "Zaheed Hossain Molla",
+      title: "Full Stack Developer",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "",
+      img: "/react-js.svg",
+      nameImg: "",
+    },
+    {
+      id: 2,
+      name: "",
+      img: "/node-js.svg",
+      nameImg: "/",
+    },
+    {
+      id: 3,
+      name: "",
+      img: "/tailwind-css.svg",
+      nameImg: "/",
+    },
+    {
+      id: 4,
+      name: "",
+      img: "/javascript.svg",
+      nameImg: "",
+    },
+    {
+      id: 5,
+      name: "",
+      img: "/typescript.svg",
+      nameImg: "",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Freshers",
+      desc: "Web Developer, Looking for an internship based on full stack web development.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Full Stack Developer",
+      desc: "Can do Mobile resposive webiste",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    // {
+    //   id: 3,
+    //   title: "Freelance App Dev Project",
+    //   desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    //   className: "md:col-span-2", // change to md:col-span-2
+    //   thumbnail: "/exp3.svg",
+    // },
+    // {
+    //   id: 4,
+    //   title: "Lead Frontend Developer",
+    //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    //   className: "md:col-span-2",
+    //   thumbnail: "/exp4.svg",
+    // },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: "https://github.com/ZAHEED-MOLLA",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+      link: "https://x.com/zaheedhossain1"
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      link: "https://www.linkedin.com/in/zaheed-hossain-molla-9b17b4250/"
+    },
+  ];
