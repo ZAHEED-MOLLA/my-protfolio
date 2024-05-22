6579d6f741e7caaa85f3c9e5c9b422bde3cce661
ZAHEED-MOLLA <zaheedhossain06@gmail.com>
2024-05-22 15:55:25 +0530

    Final Changes

--- components/ui/InfiniteMovingCard.tsx
+++ components/ui/InfiniteMovingCard.tsx
@@ -106,7 +106,7 @@ export const InfiniteMovingCards = ({
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 <span className="flex flex-col gap-1">
                     <div className="me-3">
-                        <img className="rounded-full w-20 h-20" src="/me.jpeg" alt="profile" />
+                        <img className="rounded-full w-20 h-20" src="./profile.jpeg" alt="profile" />
                     </div>
                     <div className="flex flex-col gap-1">
                     <span className=" text-xl leading-[1.6] text-white font-bold">
